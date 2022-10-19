29d2562bc8124e9bdb7efcf2fea26b080ca931f6
MamNonDevOps <112860107+MamNonDevOps@users.noreply.github.com>
2022-10-19 19:21:40 +0700

    Update ewoks.js

--- scripts/ewoks.js
+++ scripts/ewoks.js
@@ -4,13 +4,13 @@ import { check, sleep } from "k6";
 export let options = {
   stages: [
       // Ramp-up from 1 to 5 virtual users (VUs) in 5s
-      { duration: "5s", target: 5 },
+      { duration: "10s", target: 1000 },
 
       // Stay at rest on 5 VUs for 10s
-      { duration: "10s", target: 5 },
+      { duration: "20s", target: 2000 },
 
       // Ramp-down from 5 to 0 VUs for 5s
-      { duration: "5s", target: 0 }
+      { duration: "10s", target: 1000 }
   ]
 };
 
